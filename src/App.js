a92d15c104752efcb661fdd63e02a998bc65d064
Shikhara17 <pagadalashikhara@gmail.com>
2023-08-18 16:02:26 +0530

    Adding a new component - Expense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expense from "./components/Expense";
 function App() {
-  const expenses = [
+  const data = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -27,21 +26,7 @@ function App() {
       <h2>Let's get started!</h2>
       {/* Syntax can be used as object destructring while calling and syntx 2 */}
       {/* <ExpenseItem expense={expenses[0]}></ExpenseItem> */}
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expense expenses={data}></Expense>
     </div>
   );
 }
