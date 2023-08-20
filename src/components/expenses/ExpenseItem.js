1ac8f4e76879e389143a1665ddf05522b9df07c8
Shikhara17 <pagadalashikhara@gmail.com>
2023-08-20 16:48:40 +0530

    Updating the value- working with states

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React,{useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
@@ -5,14 +7,19 @@ const ExpenseItem=(props)=>{
   // const expenseDate = new Date(2023, 8, 17);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 6912;
-
+  const [title,setTitle]=useState(props.title);
+  
+  const clickHandler=()=>{
+    setTitle('updated');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense=item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
